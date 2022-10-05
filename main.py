2e3c92691edc52dcdda19d128c7b8334b660de45
MatthieuQcc <matthieu.quaccia@gmail.com>
2022-10-05 10:18:59 +0200

    starting

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import arff
 
+path = 'github.com/deric/clustering-benchmark/tree/master/src/main/resources/datasets/artificial/'
+databrut = arff.loadarff(open(path + "xclara.arff", 'r'))
+data = [[x[0], x[1]] for x in databrut[0]]
+
+# Affichage en 2D
+f0 = [f[0] for f in data]
+f1 = [f[1] for f in data]
+
+plt.scatter(f0, f1, s=8)
+plt.title("Données initiales")
+plt.show
